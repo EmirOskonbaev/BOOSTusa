62bea06448d94ea38f18d4387f7e7447d00a8dcd
EmirOskonbaev <emiroskonbaev3@gmail.com>
2021-11-26 16:21:18 +0600

    first comit

---
+++ src/js/srcipt.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        item: 3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
